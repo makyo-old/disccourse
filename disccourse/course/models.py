3b7b50214e8c415303337de08dba72fd29d59fea
Matthew Scott <mjs@mjs-svc.com>
2012-06-18 19:19:30 -0600

    Some work

--- disccourse/course/models.py
+++ disccourse/course/models.py
@@ -11,9 +11,43 @@ class Course(models.Model):
 class Hole(models.Model):
     course = models.ForeignKey(Course)
     hole_number = models.IntegerField()
-    tee = models.ForeignKey(Location)
     hole = models.ForeignKey(Location)
     par = models.IntegerField(default = 1)
     punishment_hole = models.BooleanField(default = False)
     description = models.TextField(blank = True)
     additional_rules = models.TextField(blank = True)
+
+class Tee(models.Model):
+    hole = models.ForeignKey(Hole)
+    location = models.ForeignKey(Location)
+    weight = models.IntegerField()
+
+class Hazard(models.Model):
+    HAZARD_CHOICES = (
+            ('wp', "Water - Pond"),
+            ('wl', "Water - Lake"),
+            ('wo', "Water - Ocean"),
+            ('ws', "Water - Stream"),
+            ('wr', "Water - River"),
+            ('wc', "Water - Canal"),
+            ('bl', "Building - Low"),
+            ('bh', "Building - High"),
+            ('td', "Tree - Deciduous"),
+            ('tp', "Tree - Pine"),
+            ('r1', "Rough Terrain - 1 - Manageable"),
+            ('r2', "Rough Terrain - 2 - Expert"), 
+            ('r2', "Rough Terrain - 3 - Impossible")
+            )
+    GEOMETRY_CHOICES = (
+            ('p', 'Point'),
+            ('l', 'Line'),
+            ('g', 'Polygon'),
+            )
+    hole = models.ForeignKey(Hole)
+    hazard_type = models.CharField(max_length = 2, choices = HAZARD_CHOICES)
+    geometry_type = models.CharField(max_length = 1, choices = GEOMETRY_CHOICES)
+
+class HazardGeometry(models.Model):
+    hazard = models.ForeignKey(Hazard)
+    location = models.ForeignKey(Location)
+    weight = models.IntegerField()
--- disccourse/location/views.py
+++ disccourse/location/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from disccourse.location.models import *
+
+def create(request):
+    pass
+
+def read(request, location_id):
+    pass
+
+def update(request, location_id):
+    pass
+
+def delete(request, location_id):
+    pass
