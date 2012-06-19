cf682a01cbb0d24513de02439c7fe84f3b09312a
Matthew Scott <mjs@mjs-svc.com>
2012-06-18 20:14:04 -0600

    Work

--- disccourse/location/models.py
+++ disccourse/location/models.py
@@ -1,5 +1,10 @@
 from django.contrib.gis.db import models
 
 class Location(models.Model):
+    accuracy = models.DecimalField(max_digits = 8, decimal_places = 4)
+    heading = models.DecimalField(max_digits = 8, decimal_places = 4)
+    speed = models.DecimalField(max_digits = 8, decimal_places = 4)
+    altitude = models.DecimalField(max_digits = 8, decimal_paces = 4)
+    altitude_accuracy = models.IntegerField(max_digits = 8, decimal_places = 4)
     point = models.PointField()
     objects = models.GeoManager()
