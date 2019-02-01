b0190d2878772f970babaef7d2851954a1c135c6
Madison Scott-Clary <madison@archive.org>
2019-02-01 14:03:22 -0800

    login required

--- location/views.py
+++ location/views.py
@@ -1,13 +1,24 @@
 from disccourse.location.models import *
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
+@login_required
 def create(request):
-    pass
+    if request.method == "GET":
+        return render_to_response("location/create.html", context_instance = RequestContext(request, {}))
+    else:
 
+        # redirect to read
+
+@login_required
 def read(request, location_id):
     pass
 
+@login_required
 def update(request, location_id):
     pass
 
+@login_required
 def delete(request, location_id):
     pass
